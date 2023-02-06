6b4242641a8c80d65b021e0943cfc9c7f9d4d9a5
Tysuiku <Timothydong41@gmail.com>
2023-02-06 15:34:24 -0500

    finish hash set

--- skeleton/lib/p03_hash_set.rb
+++ skeleton/lib/p03_hash_set.rb
@@ -7,12 +7,24 @@ class HashSet
   end
 
   def insert(key)
+    if num_buckets == count
+      resize!
+    end
+    if !include?(key)
+      @store[key.hash % num_buckets] << key.hash
+      @count += 1
+    end
   end
 
   def include?(key)
+    @store[key.hash % num_buckets].include?(key.hash)
   end
 
   def remove(key)
+    if include?(key)
+      @store[key.hash % num_buckets].delete(key.hash)
+      @count -= 1
+    end
   end
 
   private
@@ -26,5 +38,10 @@ class HashSet
   end
 
   def resize!
+    res = Array.new(num_buckets * 2) { Array.new }
+    @store.flatten.each do |n|
+      res[n % (num_buckets * 2)] << n
+    end
+    @store = res
   end
 end
